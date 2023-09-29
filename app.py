060ddeea38c9c67156b37cd179ed88d57a7b30a2
Sergiomeono81 <146167633+Sergiomeono81@users.noreply.github.com>
2023-09-29 00:56:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,54 @@
-import streamlit as st
+# Importa las bibliotecas necesarias
 import numpy as np
+import streamlit as st
 import matplotlib.pyplot as plt
 
-# [No hay cambios en las funciones perinola, simulacion_montecarlo, tiene_ganador, juegos_promedio_ganador, juegos_promedio_bancarrota_vs_jugadores]
+# ... [Tus definiciones de función permanecen sin cambios]
 
 def main():
-    st.title("Simulación de Perinola con Montecarlo")
-
+    st.title("Simulación Montecarlo de Perinola")
+    
+    # Configura los widgets para la interactividad
     n_jugadores = st.slider("Número de jugadores", 2, 10, 3)
     m_juegos = st.slider("Número de juegos", 10, 100, 50)
     dinero_inicial = st.slider("Dinero inicial", 5, 20, 10)
-
-    if st.button("Graficar"):
-        graficar(n_jugadores, m_juegos, dinero_inicial)
-        
-        _, _, juegos_bancarrota = simulacion_montecarlo(n_jugadores, m_juegos, dinero_inicial)
-        st.write(f"Se necesitan {juegos_bancarrota} juegos para que un jugador se quede sin dinero.")
-
-        juegos_ganador = juegos_promedio_ganador(n_jugadores, m_juegos, dinero_inicial)
-        st.write(f"En promedio, hay un ganador en {juegos_ganador} juegos.")
-
-        juegos_promedio = juegos_promedio_bancarrota_vs_jugadores(10, m_juegos, dinero_inicial)
-        plt.figure(figsize=(12, 8))
-        plt.plot(range(2, 11), juegos_promedio)
-        plt.xlabel('Número de Jugadores')
-        plt.ylabel('Juegos Promedio hasta Bancarrota')
-        plt.title('Juegos Promedio hasta Bancarrota vs. Número de Jugadores')
-        plt.grid(True)
-        st.pyplot()
-
-
-def graficar(n_jugadores, m_juegos, dinero_inicial):
+    
+    # Grafica la simulación
     jugadores, pozos, juegos_para_bancarrota = simulacion_montecarlo(n_jugadores, m_juegos, dinero_inicial)
-    plt.figure(figsize=(12, 8))
-
+    fig, ax = plt.subplots(figsize=(12, 8))
+    
     for i, dinero in enumerate(jugadores):
-        plt.plot(range(m_juegos), [dinero_inicial] * m_juegos, label=f'Jugador {i + 1}')
+        ax.plot(range(m_juegos), [dinero_inicial] * m_juegos, label=f'Jugador {i + 1}')
+    ax.plot(range(m_juegos), pozos, label='Pozo', linestyle='--')
+    ax.axvline(juegos_para_bancarrota, color='red', linestyle='--', label='Jugador en bancarrota')
+    ax.set_xlabel('Juegos')
+    ax.set_ylabel('Dinero')
+    ax.set_title('Ganancia y Pérdida por Jugador en cada Juego')
+    ax.legend()
+    ax.grid(True)
+    
+    st.pyplot(fig)
+    
+    # Responde a las preguntas
+    st.subheader("Análisis:")
+    
+    # 1. ¿Cuántos juegos son necesarios para que un jugador se quede sin dinero?
+    st.write(f"Se necesitan {juegos_bancarrota} juegos para que un jugador se quede sin dinero.")
 
-    plt.plot(range(m_juegos), pozos, label='Pozo', linestyle='--')
+    # 2. ¿En cuántos juegos en promedio hay un ganador?
+    juegos_ganador = juegos_promedio_ganador(n_jugadores, m_juegos, dinero_inicial)
+    st.write(f"En promedio, hay un ganador en {juegos_ganador} juegos.")
 
-    plt.axvline(juegos_para_bancarrota, color='red', linestyle='--', label='Jugador en bancarrota')
-    plt.xlabel('Juegos')
-    plt.ylabel('Dinero')
-    plt.title('Ganancia y Pérdida por Jugador en cada Juego')
-    plt.legend()
-    plt.grid(True)
-    st.pyplot()
-
+    # 3. ¿Cómo afecta el número de jugadores al número de juegos para que un jugador se gane todo el dinero?
+    juegos_promedio = juegos_promedio_bancarrota_vs_jugadores(10, m_juegos, dinero_inicial)
+    fig, ax = plt.subplots(figsize=(12, 8))
+    ax.plot(range(2, 11), juegos_promedio)
+    ax.set_xlabel('Número de Jugadores')
+    ax.set_ylabel('Juegos Promedio hasta Bancarrota')
+    ax.set_title('Juegos Promedio hasta Bancarrota vs. Número de Jugadores')
+    ax.grid(True)
+    
+    st.pyplot(fig)
 
 if __name__ == "__main__":
     main()
-
-  
